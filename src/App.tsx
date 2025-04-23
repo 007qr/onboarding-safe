bb8f96bcf23582cfcda1b6a83c820f5fb689516b
007qr <aayushpatil558321@gmail.com>
2025-04-23 22:50:54 +0530

    fixed animation

--- src/App.tsx
+++ src/App.tsx
@@ -15,9 +15,9 @@ const App: Component = () => {
                     <Show when={flow() == "email"}>
                         <Motion.div 
                             class="flex flex-col items-center font-medium gap-[30px] absolute"
-                            initial={{ opacity: 0, y: 100, transition: { easing: 'ease-in', duration: 0.6 } }}
+                            initial={{ opacity: 0, y: 100, transition: { easing: 'ease-in', duration: .6 } }}
                             animate={{ opacity: 1, y: 0 }}
-                            exit={{ opacity: 0, scale: 1.01, y: -100, transition: { duration: 0.7, easing: [0.215, 0.61, 0.355, 1] } }}
+                            exit={{ opacity: 0, scale: 1.01, y: -100, transition: { duration: 1.25, easing: [0.16, 1, 0.29, 0.99] } }}
                             >
                             <h4 class="text-3xl font-bold">
                                 Get started with your email
@@ -25,62 +25,84 @@ const App: Component = () => {
 
                             {/* TODO: Style the input */}
                             <Input setFlow={setFlow} next="name" />
-                            <Button onClick={() => setFlow("name")}>Get Started</Button>
-
-                            <p></p>
+                            <div class="flex gap-6">
+                                <Button onClick={() => setFlow("name")}>Get Started</Button>
+                                <div class="font-normal flex items-center gap-1 text-sm">
+                                    <img src="/arrow-subdirectory.svg" class="bg-gray-200 rounded-lg p-1"/>
+                                    <p>Or Press <span class="font-semibold">Enter</span></p>
+                                </div>
+                            </div>
                         </Motion.div>
                     </Show>
                     <Show when={flow() == "name"}>
                         <Motion.div
                             class="flex flex-col items-center font-medium gap-[30px] absolute"
-                            initial={{ opacity: 0, y: 100 }}
-                            animate={{ opacity: 1, y: 0, transition: { delay: 0.2,easing: [0.215, 0.61, 0.355, 1], duration: 0.5 } }}
-                            exit={{ opacity: 0, y: -100, transition: { duration: 0.7, easing: [0.215, 0.61, 0.355, 1] } }}
+                            initial={{ opacity: 0, y: 50 }}
+                            animate={{ opacity: 1, y: 0, transition: { delay: 0.38, easing: [0.19, 1, 0.22, 1], duration: 1.2 } }}
+                            exit={{ opacity: 0, y: -100, transition: { duration: 1.25, easing: [0.16, 1, 0.29, 0.99] } }}
                         >
                             <h4 class="text-3xl font-bold">Your Full Name</h4>
                             {/* TODO: Style the input */}
                             <Input setFlow={setFlow} next="phone" />
-                            <Button>Next</Button>
+                            <div class="flex gap-6">
+                                <Button onClick={() => setFlow("name")}>Next</Button>
+                                <div class="font-normal flex items-center gap-1 text-sm">
+                                    <img src="/arrow-subdirectory.svg" class="bg-gray-200 rounded-lg p-1"/>
+                                    <p>Or Press <span class="font-semibold">Enter</span></p>
+                                </div>
+                            </div>
                         </Motion.div>
                     </Show>
 
                     <Show when={flow() == "phone"}>
                         <Motion.div
                             class="flex flex-col items-center font-medium gap-[30px] absolute"
-                            initial={{ opacity: 0, y: 100 }}
-                            animate={{ opacity: 1, y: 0, transition: { delay: 0.2,easing: [0.215, 0.61, 0.355, 1], duration: 0.5 } }}
-                            exit={{ opacity: 0, y: -100, transition: { duration: 0.7, easing: [0.215, 0.61, 0.355, 1] } }}
+                            initial={{ opacity: 0, y: 50 }}
+                            animate={{ opacity: 1, y: 0, transition: { delay: 0.38,easing: [0.19, 1, 0.22, 1], duration: 1.2 } }}
+                            exit={{ opacity: 0, y: -100, transition: { duration: 1.25, easing: [0.16, 1, 0.29, 0.99] } }}
                         >
                             <h4 class="text-3xl font-bold">Phone Number</h4>
                             {/* TODO: Style the input */}
                             <Input setFlow={setFlow} next="otp" />
-                            <Button>Next</Button>
+                            <div class="flex gap-6">
+                                <Button onClick={() => setFlow("name")}>Next</Button>
+                                <div class="font-normal flex items-center gap-1 text-sm">
+                                    <img src="/arrow-subdirectory.svg" class="bg-gray-200 rounded-lg p-1"/>
+                                    <p>Or Press <span class="font-semibold">Enter</span></p>
+                                </div>
+                            </div>
                         </Motion.div>
                     </Show>
 
                     <Show when={flow() == "otp"}>
                         <Motion.div
                             class="flex flex-col items-center font-medium gap-[30px] absolute"
-                            initial={{ opacity: 0, y: 100 }}
-                            animate={{ opacity: 1, y: 0, transition: { delay: 0.2,easing: [0.215, 0.61, 0.355, 1], duration: 0.5 } }}
-                            exit={{ opacity: 0, y: -100, transition: { duration: 0.7, easing: [0.215, 0.61, 0.355, 1] } }}
+                            initial={{ opacity: 0, y: 50 }}
+                            animate={{ opacity: 1, y: 0, transition: { delay: 0.38,easing: [0.19, 1, 0.22, 1], duration: 1.2 } }}
+                            exit={{ opacity: 0, y: -100, transition: { duration: 1.25, easing: [0.16, 1, 0.29, 0.99] } }}
                         >
                             <h4 class="text-3xl font-bold">OTP</h4>
                             {/* TODO: Style the input */}
                             <Input setFlow={setFlow} next="done" />
-                            <Button>Next</Button>
+                            <div class="flex gap-6">
+                                <Button onClick={() => setFlow("name")}>Next</Button>
+                                <div class="font-normal flex items-center gap-1 text-sm">
+                                    <img src="/arrow-subdirectory.svg" class="bg-gray-200 rounded-lg p-1"/>
+                                    <p>Or Press <span class="font-semibold">Enter</span></p>
+                                </div>
+                            </div>
                         </Motion.div>
                     </Show>
 
                     <Show when={flow() == "done"}>
                         <Motion.div
                             class="flex flex-col items-center font-medium gap-[30px] absolute"
-                            initial={{ opacity: 0, y: 100 }}
-                            animate={{ opacity: 1, y: 0, transition: { delay: 0.2,easing: [0.215, 0.61, 0.355, 1], duration: 0.5 } }}
-                            exit={{ opacity: 0, y: -100, transition: { duration: 0.7, easing: [0.215, 0.61, 0.355, 1] } }}
+                            initial={{ opacity: 0, y: 50 }}
+                            animate={{ opacity: 1, y: 0, transition: { delay: 0.38, easing: [0.19, 1, 0.22, 1], duration: 1.2 } }}
+                            exit={{ opacity: 0, y: -100, transition: { duration: 1.25, easing: [0.16, 1, 0.29, 0.99] } }}
                         >
                             <h4 class="text-3xl font-bold">Congratulations 🎊</h4>
-                            <Button>Next</Button>
+                            <Button onClick={() => setFlow("email")}>Next</Button>
                         </Motion.div>
                     </Show>
                     
